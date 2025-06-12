b0c35d4711cf57525b32afe3fa710e6096a8aea3
Venkat <vbonam@kent.edu>
2025-06-12 12:19:46 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeTableComponent } from './features/employees/components/employee-table/employee-table.component';
+import { EmployeeFormComponent } from './features/employees/components/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeTableComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
