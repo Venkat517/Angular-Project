486af0f1f0873eb95de12da89516645c9ba4e5a3
Venkat <vbonam@kent.edu>
2025-06-13 06:15:32 -0400

    create employee feature with smart and dumb parent-child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeTableComponent } from './features/employees/components/employee-table/employee-table.component';
-import { EmployeeFormComponent } from './features/employees/components/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeTableComponent,
-    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // lazy loading routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/employees/employees.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeesRoutingModule } from './employees-routing.module';
+import { EmployeeTableComponent } from '../employees/components/employee-table/employee-table.component';
+import { EmployeeFormComponent } from '../employees/components/employee-form/employee-form.component';
+import { EmployeeListPageComponent } from '../employees/containers/employee-list-page/employee-list-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    EmployeeTableComponent,
+    EmployeeFormComponent,
+    EmployeeListPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    EmployeesRoutingModule
+  ]
+})
+export class EmployeesModule { }
